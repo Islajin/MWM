dee40781b9482f7cf1c125ebb2f152cc0bb32554
Isla <Islajiin@gmail.com>
2025-04-15 20:24:10 +0900

    Initial Commit

---
+++ MWM/MWMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWMApp.swift
+//  MWM
+//
+//  Created by yeonjin on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MWMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
